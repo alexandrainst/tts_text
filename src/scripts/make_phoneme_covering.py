c6d34a704a83f697bd2b537db942b40b2a38d2d2
Anders Jess Pedersen <38854604+AJDERS@users.noreply.github.com>
2023-10-26 10:51:04 +0200

    Update src/scripts/make_phoneme_covering.py
    Co-authored-by: Dan Saattrup Nielsen <47701536+saattrupdan@users.noreply.github.com>

--- src/scripts/make_phoneme_covering.py
+++ src/scripts/make_phoneme_covering.py
@@ -14,6 +14,7 @@ from tts_text.phoneme_covering_set import make_phoneme_covering_set
 @hydra.main(config_path="../../config", config_name="config", version_base=None)
 def main(config: DictConfig) -> None:
     """Make a phoneme covering set of wikipedia articles.
+    
     Args:
         config: The Hydra config for your project.
     """
