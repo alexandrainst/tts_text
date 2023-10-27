9ddd4873e5bd3e0e4644e902493ec048521f755b
Anders Jess Pedersen <anders.j.pedersen@alexandra.dk>
2023-10-27 13:53:57 +0200

    fix: slicing

--- src/scripts/manually_filter_reddit_comments.py
+++ src/scripts/manually_filter_reddit_comments.py
@@ -93,7 +93,7 @@ def filter_reddit_dataset(output_dir, username, num_samples, start_index):
     ]
     # Manually filter the dataset, and store which person filtered each comment
     records: list[dict[str, str | int]] = list()
-    for index, sentence in enumerate(dataset):
+    for index, sentence in enumerate(dataset[:num_samples]):
         records = prompt_user(
             answer=None,
             records=records,
