9db3d4b3593924c7e10eb6f4a0295a2dddd69f81
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2023-10-27 22:54:12 +0200

    tests: Fix some tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -28,12 +28,12 @@ def test_extract_sentences(text, expected) -> None:
 
 class TestInterleaveDatasets:
     @pytest.fixture(scope="class")
-    def dataset(self) -> list[str]:
-        return ["a", "b", "c"]
+    def dataset(self):
+        yield ["a", "b", "c"]
 
     @pytest.fixture(scope="class")
-    def other_dataset(self) -> list[str]:
-        return ["d", "e", "f"]
+    def other_dataset(self):
+        yield ["d", "e", "f"]
 
     def test_interleaving_is_a_generator(self, dataset) -> None:
         assert isinstance(
@@ -46,51 +46,42 @@ class TestInterleaveDatasets:
         )
 
     def test_with_one_sampling_dataset(self, dataset) -> None:
-        assert (
-            list(
-                interleave_datasets(
-                    non_sampling_datasets=[],
-                    sampling_datasets=[dataset],
-                    sampling_probabilities=[1.0],
-                )
+        assert set(
+            interleave_datasets(
+                non_sampling_datasets=[],
+                sampling_datasets=[dataset],
+                sampling_probabilities=[1.0],
             )
-            == dataset
-        )
+        ) == set(dataset)
 
     def test_non_sampling_dataset(self, dataset) -> None:
-        assert (
-            list(
-                interleave_datasets(
-                    non_sampling_datasets=[dataset],
-                    sampling_datasets=[],
-                    sampling_probabilities=[],
-                )
+        assert set(
+            interleave_datasets(
+                non_sampling_datasets=[dataset],
+                sampling_datasets=[],
+                sampling_probabilities=[],
             )
-            == dataset
-        )
+        ) == set(dataset)
 
     def test_with_two_sampling_datasets(self, dataset, other_dataset) -> None:
         assert len(
             list(
                 interleave_datasets(
                     non_sampling_datasets=[],
-                    sampling_datasets=[dataset, dataset],
+                    sampling_datasets=[dataset, other_dataset],
                     sampling_probabilities=[0.5, 0.5],
                 )
             )
         ) >= len(dataset)
 
     def test_with_two_non_sampling_datasets(self, dataset, other_dataset) -> None:
-        assert (
-            list(
-                interleave_datasets(
-                    non_sampling_datasets=[dataset, other_dataset],
-                    sampling_datasets=[],
-                    sampling_probabilities=[],
-                )
+        assert set(
+            interleave_datasets(
+                non_sampling_datasets=[dataset, other_dataset],
+                sampling_datasets=[],
+                sampling_probabilities=[],
             )
-            == dataset + other_dataset
-        )
+        ) == set(dataset + other_dataset)
 
     def test_with_two_sampling_datasets_and_one_non_sampling_dataset(
         self, dataset, other_dataset
@@ -102,6 +93,5 @@ class TestInterleaveDatasets:
                 sampling_probabilities=[0.5, 0.5],
             )
         )
-        assert interleaved[: len(dataset)] == dataset
-        assert set(interleaved[len(dataset) :]).issubset(set(dataset + other_dataset))
+        assert set(interleaved[: len(dataset)]) == set(dataset)
         assert len(interleaved) >= len(dataset) * 2
