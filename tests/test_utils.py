7c9331d2d1d21d040aae636d9374b67876b98188
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2023-11-09 11:03:55 +0100

    tests: Add in missing `random_seed` argument

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -35,62 +35,68 @@ class TestInterleaveDatasets:
     def other_dataset(self):
         yield ["d", "e", "f"]
 
-    def test_interleaving_is_a_generator(self, dataset) -> None:
+    def test_interleaving_is_a_generator(self, dataset, cfg) -> None:
         assert isinstance(
             interleave_datasets(
                 non_sampling_datasets=[dataset],
                 sampling_datasets=[],
                 sampling_probabilities=[],
+                random_seed=cfg.random_seed,
             ),
             Generator,
         )
 
-    def test_with_one_sampling_dataset(self, dataset) -> None:
+    def test_with_one_sampling_dataset(self, dataset, cfg) -> None:
         assert set(
             interleave_datasets(
                 non_sampling_datasets=[],
                 sampling_datasets=[dataset],
                 sampling_probabilities=[1.0],
+                random_seed=cfg.random_seed,
             )
         ) == set(dataset)
 
-    def test_non_sampling_dataset(self, dataset) -> None:
+    def test_non_sampling_dataset(self, dataset, cfg) -> None:
         assert set(
             interleave_datasets(
                 non_sampling_datasets=[dataset],
                 sampling_datasets=[],
                 sampling_probabilities=[],
+                random_seed=cfg.random_seed,
             )
         ) == set(dataset)
 
-    def test_with_two_sampling_datasets(self, dataset, other_dataset) -> None:
+    def test_with_two_sampling_datasets(self, dataset, other_dataset, cfg) -> None:
         assert len(
             list(
                 interleave_datasets(
                     non_sampling_datasets=[],
                     sampling_datasets=[dataset, other_dataset],
                     sampling_probabilities=[0.5, 0.5],
+                    random_seed=cfg.random_seed,
                 )
             )
         ) >= len(dataset)
 
-    def test_with_two_non_sampling_datasets(self, dataset, other_dataset) -> None:
+    def test_with_two_non_sampling_datasets(self, dataset, other_dataset, cfg) -> None:
         assert set(
             interleave_datasets(
                 non_sampling_datasets=[dataset, other_dataset],
                 sampling_datasets=[],
                 sampling_probabilities=[],
+                random_seed=cfg.random_seed,
             )
         ) == set(dataset + other_dataset)
 
     def test_with_two_sampling_datasets_and_one_non_sampling_dataset(
-        self, dataset, other_dataset
+        self, dataset, other_dataset, cfg
     ) -> None:
         interleaved = list(
             interleave_datasets(
                 non_sampling_datasets=[dataset],
                 sampling_datasets=[dataset, other_dataset],
                 sampling_probabilities=[0.5, 0.5],
+                random_seed=cfg.random_seed,
             )
         )
         assert set(interleaved[: len(dataset)]) == set(dataset)
