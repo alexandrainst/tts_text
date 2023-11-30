2c93b79e95342e99c5bc81451e9473d327107304
Anders Jess Pedersen <anders.j.pedersen@alexandra.dk>
2023-11-30 10:38:49 +0100

    fix: retry in case we get errors when dynamically getting soup

--- src/tts_text/utils.py
+++ src/tts_text/utils.py
@@ -157,19 +157,21 @@ def get_soup(
     if not (retries is None or retries >= 0):
         raise ValueError("Number of retries must be non-negative.")
 
+    html: str = ""
     if dynamic:
         options = Options()
         options.add_argument("--headless")
         driver = webdriver.Chrome(options=options)
-        try:
-            driver.get(url=url)
-            html = driver.page_source
-        except TimeoutException:
-            logger.warning(f"Timed out while getting soup from {url}.")
-            html = ""
-        except WebDriverException:
-            logger.warning(f"Could not get soup from {url}.")
-            html = ""
+        while not html:
+            try:
+                driver.get(url=url)
+                html = driver.page_source
+            except TimeoutException:
+                logger.warning(f"Timed out while getting soup from {url}.")
+                html = ""
+            except WebDriverException:
+                logger.warning(f"Could not get soup from {url}.")
+                html = ""
     else:
         response = rq.get(url=url)
 
@@ -189,6 +191,9 @@ def get_soup(
             )
 
         html = response.text
+
+    if not html:
+        return BeautifulSoup("")
 
     soup: BeautifulSoup = BeautifulSoup("")
     if retries is None:
